5ea36eb8196a89c0c6cc7512e2b528100bfa3498
sherrywolf <daria.wieliczko@wp.pl>
2017-03-16 22:23:13 +0100

    adding gem for better viewing errors, some validation for book and editing views

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,12 @@
 class Book < ApplicationRecord
   validates :title, presence: true,
-                    length: { minimum: 5 }
+                    length: { in: 2..50 }
+  validates :author, presence: true,
+                    length: { in: 1..50 }
+  validates :genre, presence: true,
+                  length: { in: 1..15 }
+  validates :year, presence: true,
+                  :inclusion => 1500..2100
+  validates :description, presence: true,
+                  length: {in: 100..5000}
 end
