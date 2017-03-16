5e82a635cfd4b14bc113639cac63dcdf0b6319a0
sherrywolf <daria.wieliczko@wp.pl>
2017-03-16 22:57:05 +0100

    adding pagination in views and more data in seed

--- Gemfile
+++ Gemfile
@@ -49,3 +49,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'better_errors'
+gem 'will_paginate', '~> 3.1.0'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @books = Book.last(5)
+    @books = Book.last(10).reverse
   end
 end
